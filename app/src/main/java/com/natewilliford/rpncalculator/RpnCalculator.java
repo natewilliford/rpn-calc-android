3a6318009e3413f73c0b1b512092fa52a2df9236
natewilliford <nathan.williford@gmail.com>
2024-08-09 14:57:05 -0700

    Add the RpnCalculator class.
    This utility class handles all the logic and holds state for the RPN calculator. Includes test coverage with some of the examples from the exercise description.

---
+++ app/src/main/java/com/natewilliford/rpncalculator/RpnCalculator.java
@@ -0,0 +1,94 @@
+package com.natewilliford.rpncalculator;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * A Reverse Polish notation calculator. This class manages the operations and state of an RPN
+ * calculator where an operator is entered after several operands have been entered. This means a
+ * stack structure is used to save all previous operands and solutions.
+ *
+ * A new instance should be created per use as state is stored. This class is NOT thread safe.
+ *
+ * @link <a href="https://en.wikipedia.org/wiki/Reverse_Polish_notation">Wikipedia</a>
+ */
+public class RpnCalculator {
+
+    private static final String TAG = "RpnCalculator";
+
+    /** Supported operators to submit to previously added operands. */
+    public enum Operator {
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE
+    }
+
+    private final Stack<Double> operandsStack = new Stack<>();
+
+    public RpnCalculator() {}
+
+    /**
+     * Adds an operand (a decimal number) to the stack.
+     *
+     * @param operand The operand to add.
+     */
+    public void addOperand(double operand) {
+        operandsStack.push(operand);
+    }
+
+    /**
+     * Submits an operator to do a calculation. Takes the last two operands in the stack and applies
+     * the operation to them, then adds the result back to the stack.
+     *
+     * @param op The operator to apply.
+     * @return The result of the operation.
+     */
+    public double submitOperator(Operator op) {
+        if (operandsStack.size() < 2) {
+            // TODO: Add better error handling.
+            Log.e(TAG, "Not enough operators");
+            return 0;
+        }
+
+        double last = operandsStack.pop();
+        double secondLast = operandsStack.pop();
+        double result = 0;
+
+        switch (op) {
+            case PLUS:
+                result = secondLast + last;
+                break;
+            case MINUS:
+                result = secondLast - last;
+                break;
+            case MULTIPLY:
+                result = secondLast * last;
+                break;
+            case DIVIDE:
+                result = secondLast / last;
+                break;
+            default:
+                Log.e(TAG, "Invalid operator " + op);
+        }
+
+        operandsStack.push(result);
+        return result;
+    }
+
+    /** Returns the internal stack state as a list copy. */
+    public List<Double> getStack() {
+        return new ArrayList<>(operandsStack);
+    }
+
+    /**
+     * Gets the last item in the stack. This could be the most recently added operand or the result
+     * of the last operation.
+     */
+    public double getLast() {
+        return operandsStack.peek();
+    }
+}
---
+++ app/src/test/java/com/natewilliford/rpncalculator/RpnCalculatorTest.java
@@ -0,0 +1,136 @@
+package com.natewilliford.rpncalculator;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RpnCalculatorTest {
+    private static final double DECIMAL_DELTA = 0.00001;
+
+    private RpnCalculator calc;
+
+    @Before
+    public void setUp() throws Exception {
+        calc = new RpnCalculator();
+    }
+
+    @Test
+    public void addOperand_addsToStack() {
+        final List<Double> vals = Arrays.asList(3.2, 2.1, 4.2, 10.3);
+        calc.addOperand(vals.get(0));
+        calc.addOperand(vals.get(1));
+        calc.addOperand(vals.get(2));
+        calc.addOperand(vals.get(3));
+
+        assertArrayEquals(vals.toArray(), calc.getStack().toArray());
+    }
+
+    @Test
+    public void submitOperator_plus() {
+        calc.addOperand(3);
+        calc.addOperand(4);
+
+        assertEquals(7, calc.submitOperator(RpnCalculator.Operator.PLUS), DECIMAL_DELTA);
+        assertEquals(7, calc.getLast(), DECIMAL_DELTA);
+    }
+
+    @Test
+    public void submitOperator_minus() {
+        calc.addOperand(9);
+        calc.addOperand(4);
+
+        assertEquals(5, calc.submitOperator(RpnCalculator.Operator.MINUS), DECIMAL_DELTA);
+    }
+
+    @Test
+    public void submitOperator_multiply() {
+        calc.addOperand(3);
+        calc.addOperand(4);
+
+        assertEquals(12, calc.submitOperator(RpnCalculator.Operator.MULTIPLY), DECIMAL_DELTA);
+    }
+
+    @Test
+    public void submitOperator_divide() {
+        calc.addOperand(12);
+        calc.addOperand(4);
+
+        assertEquals(3, calc.submitOperator(RpnCalculator.Operator.DIVIDE), DECIMAL_DELTA);
+    }
+
+    @Test
+    public void submitOperator_popsAndPushesToStack() {
+        calc.addOperand(8);
+        calc.addOperand(3);
+        calc.addOperand(4);
+
+        assertEquals(3, calc.getStack().size());
+
+        calc.submitOperator(RpnCalculator.Operator.PLUS);
+
+        List<Double> stack = calc.getStack();
+        assertEquals(2, stack.size(), DECIMAL_DELTA);
+        assertEquals(8, stack.get(0), DECIMAL_DELTA);
+        assertEquals(7, stack.get(1), DECIMAL_DELTA);
+    }
+
+    @Test
+    public void submitOperator_example2() {
+        calc.addOperand(5);
+        calc.addOperand(5);
+        calc.addOperand(5);
+        calc.addOperand(8);
+        calc.submitOperator(RpnCalculator.Operator.PLUS);
+        calc.submitOperator(RpnCalculator.Operator.PLUS);
+        calc.submitOperator(RpnCalculator.Operator.MINUS);
+
+        assertEquals(-13, calc.getLast(), DECIMAL_DELTA);
+
+        calc.addOperand(13);
+        calc.submitOperator(RpnCalculator.Operator.PLUS);
+        assertEquals(0, calc.getLast(), DECIMAL_DELTA);
+    }
+
+    @Test
+    public void submitOperator_example3() {
+        calc.addOperand(-3);
+        assertEquals(-3, calc.getLast(), DECIMAL_DELTA);
+        calc.addOperand(-2);
+        assertEquals(-2, calc.getLast(), DECIMAL_DELTA);
+        calc.submitOperator(RpnCalculator.Operator.MULTIPLY);
+        assertEquals(6, calc.getLast(), DECIMAL_DELTA);
+        calc.addOperand(5);
+        assertEquals(5, calc.getLast(), DECIMAL_DELTA);
+        calc.submitOperator(RpnCalculator.Operator.PLUS);
+        assertEquals(11, calc.getLast(), DECIMAL_DELTA);
+    }
+
+    @Test
+    public void submitOperator_example4() {
+        calc.addOperand(5);
+        assertEquals(5, calc.getLast(), DECIMAL_DELTA);
+        calc.addOperand(9);
+        assertEquals(9, calc.getLast(), DECIMAL_DELTA);
+        calc.addOperand(1);
+        assertEquals(1, calc.getLast(), DECIMAL_DELTA);
+        calc.submitOperator(RpnCalculator.Operator.MINUS);
+        assertEquals(8, calc.getLast(), DECIMAL_DELTA);
+        calc.submitOperator(RpnCalculator.Operator.DIVIDE);
+        assertEquals(0.625, calc.getLast(), DECIMAL_DELTA);
+    }
+
+    @Test
+    public void getLastResult_getsLast() {
+        calc.addOperand(3);
+        calc.addOperand(4);
+        assertEquals(4, calc.getLast(), DECIMAL_DELTA);
+
+        calc.submitOperator(RpnCalculator.Operator.PLUS);
+        assertEquals(7, calc.getLast(), DECIMAL_DELTA);
+    }
+}
